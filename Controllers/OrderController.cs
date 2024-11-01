263004c2f202874499a74d9ebadf3e2bb2217270
Cameron Chetty <ST10251759@vcconnect.edu.za>
2024-11-02 00:33:12 +0200

    Added OrderHistory Page for Customers to view and track Orders Placed, Added an Admin Page for Admin to Processs Orders

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,88 @@
+﻿using ABCRetailers_Cameron_Chetty_CLDV6212_POE_P3.Data;
+using ABCRetailers_Cameron_Chetty_CLDV6212_POE_P3.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ABCRetailers_Cameron_Chetty_CLDV6212_POE_P3.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public OrdersController(ApplicationDBContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+   
+
+        // Admin View
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Admin()
+        {
+            var orders = await _context.Orders
+                .Include(o => o.User)
+                .ToListAsync();
+
+            var orderViewModels = orders.Select(o => new OrderAdminViewModel
+            {
+                OrderId = o.OrderId,
+                OrderDate = o.OrderDate,
+                UserEmail = o.User.Email,
+                Status = o.Status,
+                TotalPrice = (decimal)o.TotalPrice
+            }).ToList();
+
+            return View(orderViewModels);
+        }
+
+        // Process Order
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> ProcessOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            // Update the OrderStatus to "Approved"
+            order.Status = "Approved";
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Admin));
+        }
+
+
+        [Authorize(Roles = "Client,Admin")]
+        public async Task<IActionResult> OrderHistory()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var userId = await _userManager.GetUserIdAsync(user);
+
+            var orders = await _context.Orders
+                .Where(o => o.UserId == userId)
+                .Select(o => new OrderHistoryViewModel
+                {
+                    OrderId = o.OrderId,
+                    OrderDate = o.OrderDate,
+                    Status = o.Status,
+                    TotalPrice = (decimal)o.TotalPrice
+                })
+                .ToListAsync();
+
+            return View(orders);
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.OrderId == id);
+        }
+    }
+}
---
+++ Controllers/OrderRequestsController.cs
@@ -0,0 +1,45 @@
+﻿using ABCRetailers_Cameron_Chetty_CLDV6212_POE_P3.Data;
+using ABCRetailers_Cameron_Chetty_CLDV6212_POE_P3.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ABCRetailers_Cameron_Chetty_CLDV6212_POE_P3.Controllers
+{
+    public class OrderRequestsController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+
+        public OrderRequestsController(ApplicationDBContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [Authorize(Roles = "Admin")]
+        // GET: OrderRequests
+        public async Task<IActionResult> Index(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+
+            var orderRequests = from o in _context.OrderRequests.Include(o => o.Order).Include(o => o.Product)
+                                select o;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                orderRequests = orderRequests.Where(o => o.OrderId.ToString() == searchString);
+            }
+
+            return View(await orderRequests.ToListAsync());
+        }
+
+        private bool OrderRequestExists(int id)
+        {
+            return _context.OrderRequests.Any(e => e.OrderRequestId == id);
+        }
+    }
+}
