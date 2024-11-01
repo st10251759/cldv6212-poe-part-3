ca34bffc1f98ea713cbe7d710b23d8ec6921b533
Cameron Chetty <ST10251759@vcconnect.edu.za>
2024-11-01 10:31:54 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using ABCRetailers_Cameron_Chetty_CLDV6212_POE_P3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ABCRetailers_Cameron_Chetty_CLDV6212_POE_P3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ABCRetailers_Cameron_Chetty_CLDV6212_POE_P3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
